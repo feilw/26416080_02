edb3a25beafcbd37aca238373fdbecbe7e4b4b84
Fei <firstfei00@gmail.com>
2022-04-07 16:06:38 +0700

    success 2

--- bin/uts1.dart
+++ bin/uts1.dart
@@ -1,61 +1,44 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-/*Buatlah sebuah program menggunakan DART untuk membentuk pola berikut :
-NB :
-
-*.  Inputan ganjil akan menghasilkan pola berikut. 
-Inputan genap hanya akan mencetak kata yang diinput.
-
-Input :
-
-Kata : kota
-
-Jumlah : 7
-
-Output :
-
-       +
-
-    +  k  +
-
-   + o  t  a +
-
-+ k o  t  a  k +
-
-  + o  t  a +
-
-    +  k  +
-
-       +
-
-*/
-
   stdout.write("Input: ");
   String? numInput = stdin.readLineSync();
   int num = int.parse(numInput!);
 
+  double remod = num / 2;
+
   stdout.write("Kota: ");
   String? kota = stdin.readLineSync();
 
-  stdout.write("Output: $num, $kota\n");
-}
+//  stdout.write("Output: $num, $kota\n");
 
-//Length
-//kota.lengh
-
-/*  var i = 0, j = 0, k = 0, z = 1;
-
-  for (int i = 1; i <= num; i++) {
-    stdout.write("+");
-    for (int j = num - 1; j >= i; j--) {
-      stdout.write(" "); //space
-      for (int k = 1; k <= z; k++) {
-        stdout.write("*");
-      }
-      z += 2;
-      print(" ");
+  for (int i = 0; i <= remod; i++) {
+    for (int j = i; j <= remod; j++) {
+      stdout.write("  ");
     }
+    stdout.write("+ ");
+    for (int k = 1; k <= i; k++) {
+      stdout.write("${kota![i]} "); // left
+    }
+    for (int l = 1; l <= i; l++) {
+      stdout.write("${kota![i]} "); //right
+    }
+    stdout.write("+");
+    print("");
   }
-*/
 
+  for (int i = 0; i <= remod; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write("  "); // outer space
+    }
+    stdout.write("+ ");
+    for (int k = i; k <= remod; k++) {
+      stdout.write("${kota![i]} "); // left
+    }
+    for (int l = i; l <= remod; l++) {
+      stdout.write("${kota![i]} ");
+    }
+    stdout.write("+ ");
+    print("");
+  }
+}
